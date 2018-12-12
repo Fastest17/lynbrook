e358bad5c24bcf972867c0d3031a0aa61982b316
Fastest17 <kavaletsvasia@gmail.com>
2018-12-12 12:27:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+jQuery(function($){
+	$('.homapage_slider').slick({
+  		infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+ 	    arrows: false,
+  		centerMode: true,
+  		centerPadding: '0px',
+  		draggable: true
+	});
+
+	var $carousel = $('.homapage_slider');
+	$(document).on('keydown', function(e) {
+		if (e.keyCode == 37) {
+			$carousel.slick('slickPrev');
+		}
+		if (e.keyCode == 39) {
+			$carousel.slick('slickNext');
+		}
+	});
+
+
+
+});
